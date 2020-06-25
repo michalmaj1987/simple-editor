2fdde4a1626f894f91ef536361865fbc72d21888
Michal Maj <michalmaj1987.com>
2020-06-25 12:46:46 +0200

    simple editor

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,31 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const textarea = document.querySelector('.textarea--js');
+const save = document.querySelector('.save--js');
+const load = document.querySelector('.load--js');
+
+
+const currnentValue = localStorage.getItem('entry');
+
+if (currnentValue){
+    document.querySelector('.info--js').innerHTML = 'ℹ';
+}
+
+
+
+save.addEventListener('click',(e)=> {
+     e.preventDefault();
+     localStorage.setItem('entry', textarea.value);
+     if (textarea.value){
+      document.querySelector('.info--js').innerHTML = 'ℹ';
+      }  else{
+      document.querySelector('.info--js').innerHTML = '';
+
+      }
+})
+
+load.addEventListener('click',(e)=> {
+     e.preventDefault();
+     textarea.value = localStorage.getItem('entry');
+})
